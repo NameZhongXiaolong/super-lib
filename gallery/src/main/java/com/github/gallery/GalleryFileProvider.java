39c6de8598839e8fccfe8aa72908c0eb2a4fc522
ZhongXiaolong <ZhongXiaolong@81de9a65-9f9e-4406-9c2b-e51ae0be26f6>
2021-01-27 06:09:32 +0000

    FileProvider优化

---
+++ gallery/src/main/java/com/github/gallery/GalleryFileProvider.java
@@ -0,0 +1,9 @@
+package com.github.gallery;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * Created by ZhongXiaolong on 2021/1/27 1:26 PM.
+ */
+public class GalleryFileProvider extends FileProvider {
+}
