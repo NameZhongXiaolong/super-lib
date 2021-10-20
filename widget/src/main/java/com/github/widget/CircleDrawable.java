10c8cb2aca66fb8d11038252543d0c6ee7f7c241
钟小龙 <812090584@qq.com>
2021-10-20 19:57:49 +0800

    优化圆形Drawable

--- widget/src/main/java/com/github/widget/CircleDrawable.java
+++ widget/src/main/java/com/github/widget/CircleDrawable.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.Shader;
@@ -24,7 +25,7 @@ import androidx.annotation.Nullable;
 class CircleDrawable extends Drawable {
 
     private final Paint mBitmapPaint;
-    private final int mSize; //宽/高，直径
+    private final int mBitmapSize, mCircleStrokeSize; //宽/高，直径
     private final Paint mStrokePaint;
 
     public static Bitmap drawableToBitmap(Drawable drawable) {
@@ -42,33 +43,52 @@ class CircleDrawable extends Drawable {
         return bitmap;
     }
 
-    public CircleDrawable(Drawable drawable,int circleStrokeSize, @ColorInt int circleStrokeColor) {
+    public CircleDrawable(Drawable drawable, int circleStrokeSize, @ColorInt int circleStrokeColor) {
         this(drawableToBitmap(drawable), circleStrokeSize, circleStrokeColor);
     }
 
     public CircleDrawable(Bitmap bitmap, int circleStrokeSize, @ColorInt int circleStrokeColor) {
+        mCircleStrokeSize = circleStrokeSize;
+
         //着色器 水平和竖直都需要填充满
         BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
-        mBitmapPaint = new Paint();
-        mBitmapPaint.setAntiAlias(true);
-        mBitmapPaint.setShader(bitmapShader);
         //宽高的最小值作为直径
-        mSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        mBitmapSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        mBitmapPaint = new Paint();
+        mBitmapPaint.setStyle(Paint.Style.FILL);
+        mBitmapPaint.setAntiAlias(true);
+        Matrix matrix = new Matrix();
+//        float scale = (mBitmapSize + mCircleStrokeSize * 2.0f) / mBitmapSize;
+        float sx = (getIntrinsicWidth() * 1.0f) / mBitmapSize;
+        float sy = (getIntrinsicHeight() * 1.0f) / mBitmapSize;
+        matrix.setScale(sx, sy);
+        matrix.postTranslate(mCircleStrokeSize, mCircleStrokeSize);
+        bitmapShader.setLocalMatrix(matrix);
+        mBitmapPaint.setColor(Color.GREEN);
+        mBitmapPaint.setShader(bitmapShader);
 
         mStrokePaint = new Paint();
         mStrokePaint.setStyle(Paint.Style.STROKE);
         mStrokePaint.setAntiAlias(true);
         mStrokePaint.setColor(circleStrokeColor);
-        mStrokePaint.setStrokeWidth(circleStrokeSize);
+        mStrokePaint.setStrokeWidth(mCircleStrokeSize);
     }
 
     @Override
     public void draw(@NonNull Canvas canvas) {
-        //绘制圆形Bitmap
-        canvas.drawCircle(mSize * 0.5f, mSize * 0.5f, mSize * 0.49f, mBitmapPaint);
-
         //描边
-        canvas.drawCircle(mSize * 0.5f, mSize * 0.5f, mSize * 0.49f, mStrokePaint);
+        float left = mCircleStrokeSize * 0.5f;
+        float top = mCircleStrokeSize * 0.5f;
+        float right = getIntrinsicWidth() - mCircleStrokeSize * 0.5f;
+        float bottom = getIntrinsicWidth() - mCircleStrokeSize * 0.5f;
+        canvas.drawOval(left, top, right, bottom, mStrokePaint);
+
+        //圆形Bitmap
+        left += mCircleStrokeSize * 0.5f;
+        top += mCircleStrokeSize * 0.5f;
+        right -= mCircleStrokeSize * 0.5f;
+        bottom -= mCircleStrokeSize * 0.5f;
+        canvas.drawOval(left, top, right, bottom, mBitmapPaint);
 
     }
 
@@ -91,12 +111,12 @@ class CircleDrawable extends Drawable {
     //还需要从重写以下2个方法，返回drawable实际宽高
     @Override
     public int getIntrinsicWidth() {
-        return mSize;
+        return mBitmapSize + mCircleStrokeSize * 2;
     }
 
     @Override
     public int getIntrinsicHeight() {
-        return mSize;
+        return mBitmapSize + mCircleStrokeSize * 2;
     }
 
 }
