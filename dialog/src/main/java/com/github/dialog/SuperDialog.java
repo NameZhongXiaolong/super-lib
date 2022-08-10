33e9aeb1f2970078e8124d2cd7590041e958570e
钟小龙-Mini <812090584@qq.com>
2022-08-10 11:16:10 +0800

    添加setContentView方法、完善注释

--- dialog/src/main/java/com/github/dialog/SuperDialog.java
+++ dialog/src/main/java/com/github/dialog/SuperDialog.java
@@ -115,19 +115,19 @@ public class SuperDialog extends AppCompatDialog {
     }
 
     /**
-     * 确保View已经设置了{@link View#setLayoutParams(ViewGroup.LayoutParams)}属性,否则会导致窗体宽高与实际设置的不一致
-     */
-    @Override
-    final public void setContentView(View view) {
-        super.setContentView(wrapInLayout(0, view, null));
-    }
-
-    /**
      * 可以完整的读取布局文件的属性
      */
     @Override
     final public void setContentView(int layoutResID) {
         super.setContentView(wrapInLayout(layoutResID, null, null));
+    }
+
+    /**
+     * 确保View已经设置了{@link View#setLayoutParams(ViewGroup.LayoutParams)}属性,否则会导致窗体占满整个屏幕(这是默认设置)
+     */
+    @Override
+    final public void setContentView(View view) {
+        super.setContentView(wrapInLayout(0, view, null));
     }
 
     /**
@@ -146,8 +146,25 @@ public class SuperDialog extends AppCompatDialog {
         }
     }
 
+    /**
+     * 设置窗体的布局,并且设置窗体的LayoutParams属性
+     */
     final public void setContentView(View view, FrameLayout.LayoutParams params) {
         super.setContentView(wrapInLayout(0, view, params));
+    }
+
+    /**
+     * 设置窗体的布局,并且设置窗体的宽高(位置居中)
+     */
+    final public void setContentView(View view, int width, int height) {
+        super.setContentView(wrapInLayout(0, view, new FrameLayout.LayoutParams(width, height, Gravity.CENTER)));
+    }
+
+    /**
+     * 设置窗体的布局,并且设置窗体的宽高、位置
+     */
+    final public void setContentView(View view, int width, int height, int gravity) {
+        super.setContentView(wrapInLayout(0, view, new FrameLayout.LayoutParams(width, height, gravity)));
     }
 
     private View wrapInLayout(int layoutResId, @Nullable View view, @Nullable ViewGroup.LayoutParams params) {
